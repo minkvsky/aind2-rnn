623b4e3258d17325594b5c2a8139571ab9e11f49
Unknown <weifatianxia@gmail.com>
2017-09-19 20:44:08 +0800

    complete to run

--- my_answers.py
+++ my_answers.py
@@ -12,23 +12,35 @@ def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
     y = []
+    
+    for i in range(0,len(series) - window_size):
+        X.append(series[i:i + window_size])
+        
+    y = series[window_size:]
 
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
     y.shape = (len(y),1)
-
+    
     return X,y
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
-
-
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size,1)))
+    model.add(Dense(1))
+    
+    return(model)
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
+    text = text.replace('\n', ' ')
+    text = text.replace('\r', ' ')
+    text = text.replace('\ufeff', ' ')
+    import re
+    text = re.sub(r'[^a-zA-Z!,.:;?\']', ' ', text)
 
     return text
 
@@ -37,10 +49,23 @@ def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
+    # containers for input/output pairs
+    series = list(text)
+    for i in range(0,len(series) - window_size, step_size):
+        inputs.append(text[i:i + window_size])
 
+    outputs = series[window_size::step_size]
+
+    
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    from keras.layers import Dense, Activation, LSTM
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size,num_chars)))
+    model.add(Dense(num_chars), activation='linear')
+    model.add(Activation('softmax'))
+    
+    return(model)
